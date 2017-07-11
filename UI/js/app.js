3cab1ae6dee783b156a8c769e18473ffc97dd85a
sandra-widmark <sandra.widmark@gmail.com>
2017-07-11 11:12:20 +0200

    update js and data file for ui

--- UI/js/app.js
+++ UI/js/app.js
@@ -1,18 +1,17 @@
 $(document).ready(function(){
     
   var tooltip = $( ".tool-tip" );
-  
+  var infoHeading = $(".disability-info-heading"); 
+  var infoParagraph = $( ".disability-info-paragraph" ); 
+  var adviceList = $( ".advice-list" ); 
+  var moreInfoParagraph = $( ".more-info-paragraph" ); 
+  var moreInfoHeading = $( ".more-info-heading" ); 
   
   //menu button click
   
   $(".menu-btn").click(function(){
 
     var $this = $(this); 
-    var infoHeading = $(".disability-info-heading"); 
-    var infoParagraph = $( ".disability-info-paragraph" ); 
-    var adviceList = $( ".advice-list" ); 
-    var moreInfoParagraph = $( ".more-info-paragraph" ); 
-    var moreInfoHeading = $( ".more-info-heading" ); 
    
     infoHeading.empty();
     infoParagraph.empty();
@@ -28,75 +27,18 @@ $(document).ready(function(){
 
     infoHeading.append($(this).text());
 
-    //dyslexia
+    var  id = $(this).attr( "id" );
 
-    if ($(this).hasClass( "dyslexia" )) {
-      infoParagraph.append(dyslexia.paragraph);
+    infoParagraph.append(data[id]);
       
-      $.each( dyslexia.listitems, function( key, value ) {
-        adviceList.append('<li>' + value + '</li>');
-      });
+    $.each( data[id + "-listItems"], function( key, value ) {
+      adviceList.append('<li>' + value + '</li>');
+    });
+
+    if(data[id + "-moreInfo"]) {
+      moreInfoHeading.show();
+      moreInfoParagraph.append(data[id + "-moreInfo"]);
     }
-
-    //parkinsons
-
-    if ($(this).hasClass( "parkinsons" )) {
-      infoParagraph.append(parkinsons.paragraph);
-      
-      $.each( parkinsons.listitems, function( key, value ) {
-        adviceList.append('<li>' + value + '</li>');
-      });
-      moreInfoHeading.show();
-      moreInfoParagraph.append(parkinsons.moreInfo);
-    } 
-
-    //color blindness
-
-    if ($(this).hasClass( "colorBlindness" )) {
-      infoParagraph.append(colorBlindness.paragraph);
-      
-      $.each( colorBlindness.listitems, function( key, value ) {
-        adviceList.append('<li>' + value + '</li>');
-      });
-    } 
-
-    //yellow blue color blindness
-
-    if ($(this).hasClass( "yellowBlueColorBlindness" )) {
-      infoParagraph.append(yellowBlueColorBlindness.paragraph);
-      
-      $.each(yellowBlueColorBlindness.listitems, function( key, value ) {
-        adviceList.append('<li>' + value + '</li>');
-      });
-      moreInfoHeading.show();
-      moreInfoParagraph.append(yellowBlueColorBlindness.moreInfo);
-    } 
-
-    //farsightedness
-
-    if ($(this).hasClass( "farsightedness" )) {
-      infoParagraph.append(farsightedness.paragraph);
-      
-      $.each(farsightedness.listitems, function( key, value ) {
-        adviceList.append('<li>' + value + '</li>');
-      });
-      moreInfoHeading.show();
-      moreInfoParagraph.append(farsightedness.moreInfo);
-    } 
-
-    //blurry vision
-
-    if ($(this).hasClass( "blurryVision") || $(this).hasClass( "memory") || $(this).hasClass( "concentration") || $(this).hasClass( "hearing") ) {
-      infoParagraph.append(LoremIpsum.paragraph);
-      
-      $.each( LoremIpsum.listitems, function( key, value ) {
-        adviceList.append('<li>' + value + '</li>');
-      });
-    } 
-
-    if ($(this).hasClass( "blurryVision" )) {
-      showBlurr();
-    } 
     
   });
 
@@ -112,22 +54,7 @@ $(document).ready(function(){
     if ($(".dropdown-menu").hasClass("dropdown-menu-sight")){
       $("#selected").text("Syn");
     }
-    
-    resetCSS(); 
+
   });
 
 });
-
-//content scripts
-
-function showBlurr(e) {
-  chrome.tabs.insertCSS({
-    file : "css/blurry-vision.css"
-  });
-}
-
-function resetCSS(e) {
-  chrome.tabs.insertCSS({
-    file : "css/reset.css"
-  });
-}
