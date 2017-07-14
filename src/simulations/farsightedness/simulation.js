153ecc0a7f4ce251252e3733b529877468f624aa
sandra-widmark <sandra.widmark@gmail.com>
2017-07-14 09:56:07 +0200

    change path to css in farsighted simulation

--- src/simulations/farsightedness/simulation.js
+++ src/simulations/farsightedness/simulation.js
@@ -5,12 +5,12 @@ export var port = 3000
  export function showBlurr(e) {
     console.log('import funkade!'); 
     chrome.tabs.insertCSS({
-      file : "../../src/simulations/farsightedness/css/simulation.css"
+      file : "css/simulation.css"
     });
   }
 
   function resetCSS(e) {
     chrome.tabs.insertCSS({
-      file : "~/simulations/farsightedness/css/reset.css"
+      file : "css/reset.css"
     });
   }
