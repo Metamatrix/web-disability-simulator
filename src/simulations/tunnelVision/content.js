95449725386932696a030784ef65e46bd636b87e
sandra-widmark <sandra.widmark@gmail.com>
2017-08-14 16:48:55 +0200

    add canvas for tunnel vision simulation

--- src/simulations/tunnelVision/content.js
+++ src/simulations/tunnelVision/content.js
@@ -1,4 +1,42 @@
-    var span = document.createElement('span');
+
+//create canvas element
+
+var canvas = document.createElement('canvas'); 
+canvas.setAttribute('width', document.documentElement.clientWidth);
+canvas.setAttribute('height', document.documentElement.clientHeight);
+document.body.appendChild(canvas); 
+
+var mouseX = 200;
+var mouseY = 200;
+var size = 100;
+
+var context = canvas.getContext('2d');
+
+context.clearRect(0,0,document.documentElement.clientWidth, document.documentElement.clientHeight);
+
+context.fillStyle = 'black';
+context.rect(0,0,document.documentElement.clientWidth, document.documentElement.clientHeight);
+context.fill();
+
+//create transparent circle
+
+context.globalCompositeOperation='destination-out';
+context.beginPath();
+context.arc(mouseX, mouseY, size, 0, 2 * Math.PI, false);
+context.fill();
+
+
+document.onmousemove = moveCircle; 
+
+
+$(window).on('mousemove', function(e) {
+  console.log(e.clientX, e.pageX)
+});
+
+
+
+
+/*    var span = document.createElement('span');
     span.setAttribute('id', 'circle'); 
     document.body.appendChild(span); 
 
--- src/simulations/tunnelVision/simulation.js
+++ src/simulations/tunnelVision/simulation.js
@@ -3,6 +3,10 @@ export function tunnelVision(e) {
 
     chrome.tabs.executeScript( {file: 'src/simulations/tunnelVision/content.js'} );
 
+    chrome.tabs.executeScript(null, {file:'src/UI/vendor/jquery/jquery-2.0.3.min.js'}, function(result){
+    chrome.tabs.executeScript(null, {file:'src/simulations/tunnelVision/content.js'});
+    });
+
     chrome.tabs.insertCSS({
       file : "src/simulations/tunnelVision/css/simulation.css"
     });
