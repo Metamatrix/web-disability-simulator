a226b8c8861380b7e91498c9ee35ca98f45fd4e9
sandra-widmark <sandra.widmark@gmail.com>
2017-08-23 16:00:24 +0200

    rewrite function for color blindness simulation

--- src/simulations/redGreenColorBlindness/content.js
+++ src/simulations/redGreenColorBlindness/content.js
@@ -1,14 +1,3 @@
-var allElements = $("*");
-
-allElements.each(function(index) {
-  
-  var element = allElements[index].nodeName.toLowerCase();
-
-  var elementSelector = document.querySelector('' + element + '');
-
-  elementSelector.style.filter = "url(#protanopia)"; 
-
-});
 
 $.get(chrome.extension.getURL('/simulations/redGreenColorBlindness/img/filters.svg'), function(data) {
     $(data).appendTo('body');
