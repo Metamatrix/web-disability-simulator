1d1035f166767950b133f8c9e9490bcc444d0ee9
sandra-widmark <sandra.widmark@gmail.com>
2017-08-23 15:49:19 +0200

    rewrite function for color blindness simulation

--- dist/simulations/redGreenColorBlindness/content.js
+++ dist/simulations/redGreenColorBlindness/content.js
@@ -1,26 +1,12 @@
 var allElements = $("*");
 
 allElements.each(function(index) {
+  
   var element = allElements[index].nodeName.toLowerCase();
-  var selector= $(element);
 
-  var backgroundColor = selector.css("background-color");
-  var color = selector.css("color");
-  var borderColor = selector.css("border-color"); 
+  var elementSelector = document.querySelector('' + element + '');
 
-  var resultBackgroundColor = dalto(backgroundColor, 'Protanopia');
-  var resultColor = dalto(color, 'Protanopia');
-  var resultBorderColor = dalto(borderColor, 'Protanopia');
-
-  selector.css("background-color", resultBackgroundColor);
-  selector.css("color", resultColor);
-  selector.css("border-color", resultBorderColor);
-
-  var elementsWithBG = selector.filter(function(){
-    if (this.style.backgroundImage) {
-      selector.css("filter", "url(#protanopia)");
-    }
-  });
+  elementSelector.style.filter = "url(#protanopia)"; 
 
 });
 
