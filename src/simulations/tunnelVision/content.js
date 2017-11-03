0d2315b87e8efce510aac32a2ec27fcac61dbb47
sandra-widmark <sandra.widmark@gmail.com>
2017-11-03 11:22:50 +0100

    add prefixes to css id's and clean up js for tunnel vision simulation

--- src/simulations/tunnelVision/content.js
+++ src/simulations/tunnelVision/content.js
@@ -1,21 +1,20 @@
 //create canvas element
 
-if (document.body.contains(document.querySelector('#tunnelVisionCanvas'))) {
+if (document.body.contains(document.querySelector('#wds-tunnelVisionCanvas'))) {
   document.querySelector('canvas').remove();
-};
+}
 
-var canvas = document.createElement('canvas'); 
+const canvas = document.createElement('canvas'); 
 canvas.setAttribute('width', document.documentElement.clientWidth);
 canvas.setAttribute('height', document.documentElement.clientHeight);
-canvas.setAttribute('id', 'tunnelVisionCanvas');
+canvas.setAttribute('id', 'wds-tunnelVisionCanvas');
 document.body.appendChild(canvas); 
 
-var context = canvas.getContext('2d');
+const context = canvas.getContext('2d');
 
-var canvasPos = getPosition(canvas);
-var mouseX = 1000;
-var mouseY = 500;
-var size = Math.min(window.innerWidth*0.3, 300);
+let mouseX = 1000;
+let mouseY = 500;
+let size = Math.min(window.innerWidth*0.3, 300);
 
 window.addEventListener("resize", setSize, false);
 
@@ -35,38 +34,23 @@ function setMousePosition(e) {
 }
 
 function update() {
- context.clearRect(0,0,window.innerWidth,window.innerHeight);
- 
- context.fillStyle = 'black';
- context.rect(0,0,window.innerWidth,window.innerHeight);
- context.fill();
+  context.clearRect(0,0,window.innerWidth,window.innerHeight);
+  
+  context.fillStyle = 'black';
+  context.rect(0,0,window.innerWidth,window.innerHeight);
+  context.fill();
 
- context.save();
- 
- context.globalCompositeOperation='destination-out';
- 
- var gradient = context.createRadialGradient(mouseX, mouseY, size / 2, mouseX, mouseY, 0);
- gradient.addColorStop(0, 'transparent');
- gradient.addColorStop(.25, 'white');
- gradient.addColorStop(1, 'white');
- context.fillStyle = gradient;
- context.fillRect(mouseX - size / 2, mouseY - size / 2, size, size);
- 
- context.restore();
+  context.save();
+  
+  context.globalCompositeOperation='destination-out';
+  
+  const gradient = context.createRadialGradient(mouseX, mouseY, size / 2, mouseX, mouseY, 0);
+  gradient.addColorStop(0, 'transparent');
+  gradient.addColorStop(.25, 'white');
+  gradient.addColorStop(1, 'white');
+  context.fillStyle = gradient;
+  context.fillRect(mouseX - size / 2, mouseY - size / 2, size, size);
+  
+  context.restore();
 }
 update(); 
-
-function getPosition(el) {
-  var xPosition = 0;
-  var yPosition = 0;
- 
-  while (el) {
-    xPosition += (el.offsetLeft - el.scrollLeft + el.clientLeft);
-    yPosition += (el.offsetTop - el.scrollTop + el.clientTop);
-    el = el.offsetParent;
-  }
-  return {
-    x: xPosition,
-    y: yPosition
-  };
-}
--- src/simulations/tunnelVision/index.js
+++ src/simulations/tunnelVision/index.js
@@ -1,11 +1,11 @@
 export function tunnelVision(e) {
-    console.log('tunnelseende'); 
 
     chrome.tabs.executeScript( {file: 'simulations/tunnelVision/content.js'} );
 
     chrome.tabs.insertCSS({
       file : "simulations/tunnelVision/css/main.css"
     });
+    
   }
 
 
