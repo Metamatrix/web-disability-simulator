1acc70346914d8582ab818ffa717b6b0ef867c51
sandra-widmark <sandra.widmark@gmail.com>
2017-11-09 10:19:06 +0100

    inject css for loading modal through content script

--- src/simulations/general/loading/index.js
+++ src/simulations/general/loading/index.js
@@ -1,3 +1,5 @@
 export function loadingModal() {
   chrome.tabs.executeScript( {file: 'simulations/general/loading/content.js'} );
+
+  chrome.tabs.insertCSS( {file: 'simulations/general/loading/main.css'} );
 }
