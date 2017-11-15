82d10165ddc60e3163855b0bdbfd4b85f99e2242
sandra-widmark <sandra.widmark@gmail.com>
2017-11-15 17:21:09 +0100

    add concentration simulation functions

--- dist/simulations/concentration/content.js
+++ dist/simulations/concentration/content.js
@@ -1,22 +1,73 @@
-console.log('this is the concentration script!'); 
-
-
 (function(){
 
-  const circle = document.createElement('div'); 
+  const circleElement = document.createElement('div'); 
 
-  circle.setAttribute('id', 'wds-concentrationCircle');
-    
-  document.body.appendChild(circle); 
+  circleElement.setAttribute('id', 'wds-concentrationCircle');
 
-  var leftposition = 0
+  var width = document.documentElement.clientWidth;
+  var height = document.documentElement.clientHeight; 
 
-  function moveCircle(timestamp){
-      leftposition += 5
-      circle.style.bottom = leftposition + 'px'
-      requestAnimationFrame(moveCircle); 
+  document.body.appendChild(circleElement); 
+
+  //generate a random number
+  function random(min, max) {
+    var num = Math.floor(Math.random() * (max - min + 1)) + min;
+    return num; 
   }
 
-  requestAnimationFrame(moveCircle);
+  function Circle(x, y, velX, velY, size) {
+    this.x = x;
+    this.y = y; 
+    this.velX = velX;
+    this.velY = velY; 
+    this.size = size; 
+  }
+
+  Circle.prototype.move = function() {
+    circleElement.style.left= this.x + 'px'
+    circleElement.style.top= this.y + 'px'
+  }
+
+  Circle.prototype.update = function() {
+    if ((this.x + this.size) >= width) {
+      this.velX = -(this.velX);
+    }
+
+    if ((this.x - this.size) <= 0) {
+      this.velX = -(this.velX);
+    }
+
+    if ((this.y + this.size) >= height) {
+      this.velY = -(this.velY);
+    }
+
+    if ((this.y - this.size) <= 0) {
+      this.velY = -(this.velY);
+    }
+
+    this.x += this.velX;
+    this.y += this.velY;
+  }
+
+  var circle = new Circle(
+      random(0,width),
+      random(0,height),
+      15,
+      15,
+      100
+    );
+
+//animation loop
+function loop() {
+
+  circle.move();
+  circle.update();
+
+  requestAnimationFrame(loop);
+}
+loop(); 
+
 
 })()
+
+
