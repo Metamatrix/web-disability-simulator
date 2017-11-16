7af2f1bc8d5cebec9947023a9ede178db68316da
Andreas Nylin <andreas.nylin@gmail.com>
2017-11-16 13:34:11 +0100

    config changes

--- Gruntfile.js
+++ Gruntfile.js
@@ -73,7 +73,7 @@ module.exports = function(grunt) {
           {expand: true, cwd: 'src/', src: ['simulations/**'], dest: 'dist/', filter: 'isFile'},
           {expand: true, cwd: 'src/UI/', src: ['app.html'], dest: 'dist/UI', filter: 'isFile'},
           {expand: true, src: ['manifest.json'], dest: 'dist/', filter: 'isFile'},
-          {expand: true, src: ['background.js'], dest: 'dist/', filter: 'isFile'},
+          {expand: true, src: ['src/background.js'], dest: 'dist/' }
         ],
       },
     },
