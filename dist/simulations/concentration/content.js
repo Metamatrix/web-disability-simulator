beff77ebd0da9d37c170d91ab57b88f97174a672
sandra-widmark <sandra.widmark@gmail.com>
2017-11-16 09:24:48 +0100

    update concentration function code to es6

--- dist/simulations/concentration/content.js
+++ dist/simulations/concentration/content.js
@@ -1,55 +1,56 @@
-(function(){
-
-  const circleElement = document.createElement('div'); 
+((() => {
+  const circleElement = document.createElement('div');
 
   circleElement.setAttribute('id', 'wds-concentrationCircle');
 
-  var width = document.documentElement.clientWidth;
-  var height = document.documentElement.clientHeight; 
+  const width = document.documentElement.clientWidth;
+  const height = document.documentElement.clientHeight;
 
-  document.body.appendChild(circleElement); 
+  document.body.appendChild(circleElement);
 
   //generate a random number
   function random(min, max) {
-    var num = Math.floor(Math.random() * (max - min + 1)) + min;
+    const num = Math.floor(Math.random() * (max - min + 1)) + min;
     return num; 
   }
 
-  function Circle(x, y, velX, velY, size) {
-    this.x = x;
-    this.y = y; 
-    this.velX = velX;
-    this.velY = velY; 
-    this.size = size; 
+  class Circle {
+    constructor(x, y, velX, velY, size) {
+      this.x = x;
+      this.y = y; 
+      this.velX = velX;
+      this.velY = velY; 
+      this.size = size; 
+    }
+
+    move() {
+      circleElement.style.left= `${this.x}px`
+      circleElement.style.top= `${this.y}px`
+    }
+
+    update() {
+      if ((this.x + this.size) >= width) {
+        this.velX = -(this.velX);
+      }
+
+      if ((this.x - this.size) <= 0) {
+        this.velX = -(this.velX);
+      }
+
+      if ((this.y + this.size) >= height) {
+        this.velY = -(this.velY);
+      }
+
+      if ((this.y - this.size) <= 0) {
+        this.velY = -(this.velY);
+      }
+
+      this.x += this.velX;
+      this.y += this.velY;
+    }
   }
 
-  Circle.prototype.move = function() {
-    circleElement.style.left= this.x + 'px'
-    circleElement.style.top= this.y + 'px'
-  }
-
-  Circle.prototype.update = function() {
-    if ((this.x + this.size) >= width) {
-      this.velX = -(this.velX);
-    }
-
-    if ((this.x - this.size) <= 0) {
-      this.velX = -(this.velX);
-    }
-
-    if ((this.y + this.size) >= height) {
-      this.velY = -(this.velY);
-    }
-
-    if ((this.y - this.size) <= 0) {
-      this.velY = -(this.velY);
-    }
-
-    this.x += this.velX;
-    this.y += this.velY;
-  }
-
-  var circle = new Circle(
+  const circle = new Circle(
       random(0,width),
       random(0,height),
       15,
@@ -57,17 +58,15 @@
       100
     );
 
-//animation loop
-function loop() {
+  //animation loop
+  function loop() {
 
-  circle.move();
-  circle.update();
+    circle.move();
+    circle.update();
 
-  requestAnimationFrame(loop);
-}
-loop(); 
-
-
-})()
+    requestAnimationFrame(loop);
+  }
+  loop();
+}))()
 
 
