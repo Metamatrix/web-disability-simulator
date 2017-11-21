b52d650ebc513f4b42f06d211d9b0b31025a10a2
Sandra Widmark <sandra.widmark@metamatrix.se>
2017-11-21 16:46:24 +0100

    add functions for parkinsons simulation

--- dist/simulations/parkinsons/content.js
+++ dist/simulations/parkinsons/content.js
@@ -1,5 +1,5 @@
 ((() => {
-  const cursor = document.createElement('div');
+  let cursor = document.createElement('div');
 
   cursor.setAttribute('id', 'wds-parkinsonsCursor');
 
@@ -19,13 +19,25 @@
   cursor.style.background = `url(${cursorImgUrl})`
 
 
+  //generate a random number
+  function random(min, max) {
+    const num = Math.floor(Math.random() * (max - min + 1)) + min;
+    return num; 
+  }
+
   $(document).mousemove(e => {
-    $("#wds-parkinsonsCursor").css({left:e.pageX, top:e.pageY});
+
+    cursor = $("#wds-parkinsonsCursor");
+ 
+    cursor.css({left:e.pageX, top:e.pageY});
+
+    setTimeout(() => { 
+      const mousePosX = (e.pageX + random(-75,75));
+      const mousePosY = (e.pageY + random(-75,75)); 
+      cursor.css({left:mousePosX, top:mousePosY});
+    }, 500);
+    
   });
-
-
-  //Skakeffekt för muspekaren
-
 
   //Om användaren klickar på något på sidan så ska det kontrolleras om bilden är på elementet, inte muspekaren. Lite osäkert hur det ska lösas.
 
