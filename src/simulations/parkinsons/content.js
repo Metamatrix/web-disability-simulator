79d4b441f0a9b6d3eb0ae957d3e59e18d14acd91
Andreas Nylin <andreas.nylin@gmail.com>
2017-11-30 15:43:36 +0100

    Click event for parkinsons simulation

--- src/simulations/parkinsons/content.js
+++ src/simulations/parkinsons/content.js
@@ -12,25 +12,46 @@ let cursorImgUrl = '',
   posInterval = null,
   cursorPosX = 0,
   cursorPosY = 0,
+  viewportPosX = 0,
+  viewportPosY = 0,
   offsetX = 0,
   offsetY = 0,
-  css = null;
+  css = null,
+  clickedElement = null;
 
 function mousemoveHandler(e) {
-  cursorPosX = (e.pageX + offsetX);
-  cursorPosY = (e.pageY + offsetY); 
+  // Save the position of the fake cursor
+  cursorPosX = e.pageX + offsetX;
+  cursorPosY = e.pageY + offsetY;
+
+  // Save the viewport position of the fake cursor (position without scroll)
+  // We use this later to get the clicked element
+  viewportPosX = e.clientX + offsetX;
+  viewportPosY = e.clientY + offsetY;
+
   setStyle(cursor, {left: cursorPosX + 'px', top: cursorPosY + 'px', transition: 'left 0.05s, top 0.05s'});
 }
 
 function elementClickHandler(e) {
-  // TODO: Make this work
-  const currentElement = e.target,
-    elementRect = currentElement.getBoundingClientRect(),
-    clickHit = pointInRect(cursorPosX, cursorPosY, elementRect);
 
-  if(!clickHit) {
-    e.preventDefault(); 
+  if(e.target === clickedElement) {
+    // Actual mouse clicked element is the same as the element that the fake cursor would click.
+    // This is because we triggered the click or that the positions of the mouse and the fake cursor are both over the same element.
+    // Do nothing and pass on the click. Reset the clicked element.
+    clickedElement = null;
   }
+  else {
+    // Actual mouse clicked element is NOT the same as the element that the fake cursor would click.
+    // Get the element that the fake cursor would click and trigger click on that element.
+    e.preventDefault();
+
+    clickedElement = document.elementFromPoint(viewportPosX, viewportPosY);
+
+    if(clickedElement) {
+      clickedElement.click();
+    }
+  }
+
 }
 
 function setOffset() { 
@@ -54,9 +75,7 @@ function start() {
   
   document.addEventListener('mousemove', mousemoveHandler);
 
-  document.querySelectorAll('*', (el) => {
-    el.addEventListener('click', elementClickHandler);
-  });
+  document.addEventListener('click', elementClickHandler);
 
   posInterval = setInterval(setOffset, shakePositionInterval);
 
@@ -64,7 +83,9 @@ function start() {
 
 function stop() {
 
-  removeElement(cursor);
+  if(cursor) {
+    removeElement(cursor);
+  }
 
   if(css) {
     removeElement(css);
