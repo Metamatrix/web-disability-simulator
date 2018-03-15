a8ce85ea3368dc3eb15e55b06a954fdc1518a32e
sandra-widmark <sandra.widmark@gmail.com>
2018-03-15 16:44:50 +0100

    css and background script update

--- src/background.js
+++ src/background.js
@@ -1,11 +1,13 @@
-chrome.tabs.onUpdated.addListener(function () {
+chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
 
-  // Reset the icon if the page reloads or on onavigation
-  chrome.browserAction.setIcon({
-    path : "UI/img/icon.png"
-  });
+                              
+      // Reset the icon if the page reloads
+      chrome.browserAction.setIcon({
+        path : "UI/img/icon.png"
+      });
 
-  // Clear state on reload or navigation
-  chrome.storage.local.remove('activeSimulation');
+      // Clear state on reload
+      chrome.storage.local.remove('activeSimulation');
+
 
 });
